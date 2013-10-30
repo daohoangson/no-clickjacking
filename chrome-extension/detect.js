2e2cae2c8962d605d8abf6eb82658afcb6861c3e
Dao Hoang Son <daohoangson@gmail.com>
2013-10-30 18:15:52 +0700

    Google Chrome extension v1.0

---
+++ chrome-extension/detect.js
@@ -0,0 +1,81 @@
+(function()
+{
+	var count = 0;
+	var countMax = 10;
+	var intervalStep = 100;
+	var interval = intervalStep;
+
+	var nodeIdPrefix = '_no-clickjacking-';
+	var nodeIdCount = 0;
+
+	var log = function()
+	{
+		return;
+		console.log.apply(console, arguments);
+	}
+
+	var detect = function()
+	{
+		log('detecting...', count, interval);
+
+		var iframes = document.getElementsByTagName('iframe');
+		var transparentNodeIds = [];
+
+		for (var i in iframes)
+		{
+			var iframe = iframes[i];
+			var node = iframe;
+			
+			while(node)
+			{
+				var style = getComputedStyle(node);
+
+				if (style && ('' + style.opacity) === '0')
+				{
+					log('found', node, iframe);
+
+					// reset interval to check as fast as possible
+					interval = intervalStep;
+					count = 0;
+
+					var nodeId = node.id;
+					if (!nodeId)
+					{
+						nodeId = nodeIdPrefix + nodeIdCount;
+						nodeIdCount++;
+						node.id = nodeId;
+					}
+					transparentNodeIds.push(nodeId);
+				}
+
+				node = node.parentNode;
+			}
+		}
+
+		if (transparentNodeIds.length > 0)
+		{
+			var css = '';
+			for (var i in transparentNodeIds)
+			{
+				css += '#' + transparentNodeIds[i] + '{opacity:1 !important;overflow: visible !important}';
+			}
+
+			var style = document.createElement('style');
+			style.innerText = css;
+			document.getElementsByTagName('head')[0].appendChild(style);
+		}
+
+		if (count < countMax)
+		{
+			count++;
+			interval += count * intervalStep;
+			window.setTimeout(detect, interval);
+		}
+		else
+		{
+			log('stopped');
+		}
+	};
+
+	detect();
+})();
